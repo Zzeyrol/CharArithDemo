a0e93ad21d5c68d8a460b13fc084701c44fda8dd
Zzeyrol <alex_good2510@mail.ru>
2021-11-03 17:16:18 +0300

    chararithdemo

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char ch;
+        
+        ch = 'L';
+        System.out.println("ch равно " + ch);
+
+        ch++;
+        System.out.println("Значение ch изменилось на " + ch);
+
+        ch = 'Д';
+        ch--;
+        System.out.println("Значение ch снова изменилось и равно " + ch);
+    }
+}
